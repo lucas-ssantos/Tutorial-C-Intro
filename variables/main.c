4d518d619cde9216a016e79307141604366f9808
Lucas Siqueira Santos <lucas.dev.ssantos@gmail.com>
2025-08-29 07:34:06 -0300

    var float

--- variables/main.c
+++ variables/main.c
@@ -4,9 +4,12 @@ int main()
 {
     //Aprendendo sobre variáveis
 
-    int inteiro = 1;
-
-    printf("Valor da variavel inteiro é de: %d", inteiro);
+    //Inteiros
+    int inteiro = 2;
+    int inteiro2 = 3;
+    printf("-- INT\n");
+    printf("Valor da variavel inteiro é de: %d\n", inteiro);
+    printf("Valor da variavel inteiro2 é de: %d\n", inteiro2);
 
     return 0;
 }
