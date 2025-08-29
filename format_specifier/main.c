1f161b3134d490970bb145e0896da26aefbe88a6
Lucas Siqueira Santos <lucas.dev.ssantos@gmail.com>
2025-08-29 09:55:44 -0300

    Inicio Format Specifier

---
+++ format_specifier/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    //Especificação de Formato
+    //
+    // Tokens especiais que começam com o simbolo %
+    // seguidos por algum caracter específico do tipo de dado
+    // e modificadores opcionais (largura, precisão, flags).
+    // Eles controlam como o dado é mostrado e interpretado.
+}
